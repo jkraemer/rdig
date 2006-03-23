26516108d0d630d063e1d656d264e016a709b511
Jens Kraemer <jk@jkraemer.net>
2006-03-23 15:38:05 +0000

    searching

--- lib/rdig/search.rb
+++ lib/rdig/search.rb
@@ -1,6 +1,6 @@
 module RDig
-
   module Search
+
     # used to search the index
     class Searcher
       include Ferret::Search
@@ -61,42 +61,6 @@ module RDig
   #      self.extract = doc[:content][0..200]
   #    end
   #  end
-  end
 
-  module Index
-  
-    # used by the crawler to build the ferret index
-    class Indexer
-      include MonitorMixin, Ferret::Index, Ferret::Document
-      
-      def initialize(settings)
-        @ferret_config = settings
-        @index_writer = IndexWriter.new(@ferret_config.path,
-                                        :create => @ferret_config.create)
-        super() # scary, MonitorMixin won't initialize if we don't call super() here (parens matter)
-      end
-      
-      def add_to_index(document)
-        puts "add to index: #{document.uri.to_s}"
-        doc = Ferret::Document::Document.new
-        doc << Field.new("url", document.url, 
-                        Field::Store::YES, Field::Index::UNTOKENIZED)
-        doc << Field.new("title", document.title, 
-                        Field::Store::YES, Field::Index::TOKENIZED)
-        doc << Field.new("data",  document.body, 
-                        Field::Store::YES, Field::Index::TOKENIZED)
-        synchronize do
-          @index_writer << doc
-        end
-      end
-      alias :<< :add_to_index
-  
-      def close
-        @index_writer.optimize
-        @index_writer.close
-        @index_writer = nil
-      end
-    end
-    
   end
 end
